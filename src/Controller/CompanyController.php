d268b56cc357ce6f16549a7aa9ad7350cd5be1a5
Petervrancken <peter.vrancken88@gmail.com>
2024-11-03 00:06:07 +0100

    post/get/delete tested and working for company

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -6,27 +6,20 @@ namespace App\Controller;
 
 use App\Entity\Company;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Node\Name;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CompanyController extends AbstractController
 {
     #[Route('/companies', name: 'company_create', methods: ['GET', 'POST'])]
     public function handle(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
     {
-        
         if ($request->isMethod('GET')) {
-            // Handle GET request: fetch all companies
-            $companies = $entityManager->getRepository(Company::class)->findAll();
-
-            // Check if the request expects JSON (like for an API call)
-            if ($request->headers->get('Accept') === 'application/json') {
-                return new JsonResponse($companies);
-            }
+            $companies = $entityManager->getRepository(Company::class)->findBy([], ['id' => 'DESC']);
 
             return $this->render('company/index.html.twig', [
                 'companies' => $companies,
@@ -34,21 +27,12 @@ class CompanyController extends AbstractController
         }
 
         if ($request->isMethod('POST')) {
-             // Handle POST request: create a new company
-
-            // Use $request->request to get form data
             $data = $request->request->all();
 
-            // Log the submitted data for debugging
-            dump($data); // This will show the submitted form data
-            
-            // Ensure all required fields are present
             if (!isset($data['name'], $data['vatNumber'], $data['street'], $data['houseNo'], $data['busNo'], $data['postcode'], $data['city'])) {
                 return $this->json(['error' => 'Missing required fields'], Response::HTTP_BAD_REQUEST);
             }
-            
 
-            // Create a new Company entity and set its properties
             $company = new Company();
             $company->setName($data['name']); 
             $company->setVatNumber($data['vatNumber']); 
@@ -58,20 +42,39 @@ class CompanyController extends AbstractController
             $company->setPostcode($data['postcode']); 
             $company->setCity($data['city']); 
 
-            // Validate the entity (optional, but recommended)
             $errors = $validator->validate($company);
             if (count($errors) > 0) {
                 return $this->json(['error' => (string) $errors], Response::HTTP_BAD_REQUEST);
             }
 
-            // Persist and save the entity
             $entityManager->persist($company);
             $entityManager->flush();
 
-            return $this->json(['message' => 'Company created successfully'], Response::HTTP_CREATED);
+            // Add flash message for success
+            $this->addFlash('success', 'Company "' . $company->getName() . '" with VAT number ' . $company->getVatNumber() . ' was created successfully!');
+
+
+            // Redirect to the company listing page
+            return $this->redirectToRoute('company_create');
         }
 
-        // If method is not GET or POST (though it should be caught by the route config)
-        return $this->json(['error' => 'Method not allowed'], Response::HTTP_METHOD_NOT_ALLOWED);
+        return $this->json(['error' => 'Company data not available'], Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+    #[Route('/companies/delete/{id}', name: 'company_delete', methods: ['POST'])]
+    public function delete(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $company = $entityManager->getRepository(Company::class)->find($id);
+
+        if (!$company) {
+            $this->addFlash('error', 'Company not found.');
+            return $this->redirectToRoute('company_create');
+        }
+
+        $entityManager->remove($company);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Company "' . $company->getName() . '" was deleted successfully.');
+
+        return $this->redirectToRoute('company_create');
     }
 }
